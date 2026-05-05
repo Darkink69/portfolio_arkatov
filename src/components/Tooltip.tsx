47a85112e087453e8984119c1456704fa23ecf27
Vladimir Arkatov <chch69_for_trash@mail.ru>
2026-05-05 16:26:46 +0700

    research_0.1

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -17,7 +17,7 @@ const Tooltip: React.FC<TooltipProps> = ({ text, children }) => {
         {children}
       </div>
       {isVisible && (
-        <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 bg-gray-900 text-white text-sm rounded-lg border border-purple-500 whitespace-nowrap z-10 shadow-xl">
+        <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 bg-gray-900 text-white text-sm rounded-lg border border-purple-500 whitespace-normal z-10 shadow-xl">
           {text}
           <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-gray-900"></div>
         </div>
